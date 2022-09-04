9a961097da1b3266eeab10654d93398240eae8b9
Rafael Polmann <rafaelpolmann@gmail.com>
2022-09-04 18:02:22 -0300

    Impemented Result-Operation structure.

---
+++ OperationResult.Library/OptionExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace OperationResult.Library
+{
+    public static class Option
+    {
+        public static Option<T> Of<T>(T value) => new Option<T>(value, value != null);
+    }
+}
---
+++ operation-result/Program.cs
@@ -0,0 +1,20 @@
+﻿using OperationResult.Library;
+
+Result<Exception, int> result = Result.Run(OperationWithReturn);
+Result<Exception, bool> resultException = Result.Run(OperationWithException);
+
+
+Console.WriteLine($"Result with return: IsSuccess: {result.IsSuccess}  Value: {(result.IsSuccess ? result.Success : result.Failure)}");
+Console.WriteLine($"Result with Exception: IsSuccess: {resultException.IsSuccess}  Value: {(resultException.IsSuccess ? resultException.Success : resultException.Failure)}");
+
+Console.ReadKey();
+
+int OperationWithReturn()
+{
+    return 10;
+}
+
+bool OperationWithException()
+{
+    throw new Exception("Result would be an exception!!!");
+}
